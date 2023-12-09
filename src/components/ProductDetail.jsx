ed3f8c7dd29163725187291e4091b5fa4e37b805
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2023-12-10 00:18:40 +0300

    bug fixed

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -18,15 +18,15 @@ function ProductDetail({ product }) {
                         <p className="text-xl font-['Avenir-Book'] mb-4">{char}</p>
                         <div className="py-2">
                             <button
-                                className={`cursor-pointer inline-block bg-[#E6CA97] rounded-full xl:w-8 xl:h-8 lg:w-6 lg:h-6 mr-5 sm:w-4 sm:h-4 focus:ring-1 focus:ring-offset-1 focus:ring-black`}
+                                className={`cursor-pointer inline-block bg-[#E6CA97] rounded-full xl:w-8 xl:h-8 lg:w-6 lg:h-6 lg:mr-5 sm:mr-3 mr-1 w-4 h-4 focus:ring-1 focus:ring-offset-1 focus:ring-black`}
                                 onClick={() => setChar('Yellow Gold')}>
                             </button>
                             <button
-                                className={`cursor-pointer inline-block bg-[#D9D9D9] rounded-full xl:w-8 xl:h-8 lg:w-6 lg:h-6 mr-5 sm:w-4 sm:h-4 focus:ring-1 focus:ring-offset-1 focus:ring-black`}
+                                className={`cursor-pointer inline-block bg-[#D9D9D9] rounded-full xl:w-8 xl:h-8 lg:w-6 lg:h-6 lg:mr-5 sm:mr-3 mr-1 w-4 h-4 focus:ring-1 focus:ring-offset-1 focus:ring-black`}
                                 onClick={() => setChar('White Gold')}>
                             </button>
                             <button
-                                className={`cursor-pointer inline-block bg-[#E1A4A9] rounded-full xl:w-8 xl:h-8 lg:w-6 lg:h-6 sm:w-4 sm:h-4 focus:ring-1 focus:ring-offset-1 focus:ring-black`}
+                                className={`cursor-pointer inline-block bg-[#E1A4A9] rounded-full xl:w-8 xl:h-8 lg:w-6 lg:h-6 w-4 h-4 focus:ring-1 focus:ring-offset-1 focus:ring-black`}
                                 onClick={() => setChar('Rose Gold')}>
                             </button>
                         </div>
